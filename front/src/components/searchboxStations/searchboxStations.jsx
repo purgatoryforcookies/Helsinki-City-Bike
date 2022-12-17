5961654bfe715498a4c5777ce49c32550af604e7
Max <max.caroe@gmail.com>
2022-12-17 22:25:33 +0200

    navbar test added

--- front/src/components/searchboxStations/searchboxStations.jsx
+++ front/src/components/searchboxStations/searchboxStations.jsx
@@ -4,71 +4,71 @@ import ResultCard from './resultCard/resultCard'
 import "./searchboxStations.scss"
 import { Scrollbars } from 'react-custom-scrollbars-2';
 
-function SearchboxStations({name, value, onchange, success}) {
+function SearchboxStations({ name, value, onchange, success }) {
 
     const [debouncedSearch, setDebounceSearch] = useState("")
     const [close, setClose] = useState(true)
     const [search, setSearch] = useState("")
 
-    const {isError, data, isLoading} = useSearchStation(debouncedSearch)
+    const { isError, data, isLoading } = useSearchStation(debouncedSearch)
     const result_list_ref = useRef()
 
-    useEffect(()=>{
+    useEffect(() => {
         if (close || isLoading) return
 
-        const valueChange = setTimeout(()=>{
+        const valueChange = setTimeout(() => {
             setDebounceSearch(search)
         }, 500)
-        return ()=> clearTimeout(valueChange)
-    },[search])
+        return () => clearTimeout(valueChange)
+    }, [search])
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!value) {
             setSearch("")
-            }
-    },[value])
+        }
+    }, [value])
 
-    useEffect(()=>{
+    useEffect(() => {
         document.addEventListener('mousedown', handleListClose)
         return () => document.removeEventListener('mousedown', handleListClose)
     })
-    const handleListClose = (e) =>{
-        if (!result_list_ref.current.contains(e.target)){
+    const handleListClose = (e) => {
+        if (!result_list_ref.current.contains(e.target)) {
             setClose(true)
         }
     }
-    function handleClick(value){
-        onchange({target:{name:name, value:value.station_id}})
+    function handleClick(value) {
+        onchange({ target: { name: name, value: value.station_id } })
         setClose(true)
         setSearch(`${value.name} - ID: ${value.station_id}`)
 
     }
-   
+
 
     const dataToShow = !close ? data : []
-      
 
-  return (
-    <div className='searchboxStationsBody' ref={result_list_ref}>
-      <input type="text" 
-      name={name} 
-      onChange={(e)=>setSearch(e.target.value)} 
-      value={search} 
-      onClick={()=>setClose(false)}
-      autoComplete='off'/>
-      {!close && <div className="results_list" >
-      <Scrollbars style={{ width: '100%', height: 300 }}>
-      {dataToShow && dataToShow.map(item =>
-            <div key={item.station_id} onClick={()=>handleClick(item)} className="result_card">
-                <ResultCard data={item}/>
-            </div>
-        )}
-        </Scrollbars>
-        {isError && <div>Error!</div>}
 
-      </div>}
-    </div>
-  )
+    return (
+        <div className='searchboxStationsBody' ref={result_list_ref}>
+            <input type="text"
+                name={name}
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+                onClick={() => setClose(false)}
+                autoComplete='off' />
+            {!close && <div className="results_list" >
+                <Scrollbars style={{ width: '100%', height: 300 }}>
+                    {dataToShow && dataToShow.map(item =>
+                        <div key={item.station_id} onClick={() => handleClick(item)} className="result_card">
+                            <ResultCard data={item} />
+                        </div>
+                    )}
+                </Scrollbars>
+                {isError && <div>Error!</div>}
+
+            </div>}
+        </div>
+    )
 }
 
 
